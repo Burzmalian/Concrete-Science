ff584dc4631303158b069edfd3de29226643e3df
Burzmalian <gylesfohl@gmail.com>
2015-08-20 22:53:30 -0500

    reset

--- assets/prod/js/prod.js
+++ assets/prod/js/prod.js
@@ -23,23 +23,8 @@ $(document).ready(function () {
     removalDelay: 300,
     mainClass: 'mfp-fade'
   });
-
+ 
 });
-
-$(window).bind("load", function () {
-  $('.header-cta').addClass('hdr-active');
-});
-
-$(document).ready(function () {
-  checkSize();
-  $(window).resize(checkSize);
-});
-
-function checkSize() {
-  if ($(".hero-slider").css("display") == "none") {
-    $('.hero-txt-wrap').appendTo('.hero');
-  }
-}
 /*jshint browser:true */
 /*!
 * FitVids 1.1
--- assets/src/js/core.js
+++ assets/src/js/core.js
@@ -23,20 +23,5 @@ $(document).ready(function () {
     removalDelay: 300,
     mainClass: 'mfp-fade'
   });
-
+ 
 });
-
-$(window).bind("load", function () {
-  $('.header-cta').addClass('hdr-active');
-});
-
-$(document).ready(function () {
-  checkSize();
-  $(window).resize(checkSize);
-});
-
-function checkSize() {
-  if ($(".hero-slider").css("display") == "none") {
-    $('.hero-txt-wrap').appendTo('.hero');
-  }
-}
