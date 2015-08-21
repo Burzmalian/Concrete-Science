dcb5f6c3e994b6a47d51e52a61a18bf78e8bcd61
Burzmalian <gylesfohl@gmail.com>
2015-08-20 22:38:27 -0500

    slider 2

--- assets/src/js/core.js
+++ assets/src/js/core.js
@@ -31,14 +31,10 @@ $(window).bind("load", function () {
 });
 
 $(document).ready(function () {
-  // run test on initial page load
   checkSize();
-
-  // run test on resize of the window
   $(window).resize(checkSize);
 });
 
-//Function to the css rule
 function checkSize() {
   if ($(".hero-slider").css("display") == "none") {
     $('.hero-txt-wrap').appendTo('.hero');
