6d47f6d372f970e463070a4e5b7474654f974b21
Burzmalian <gylesfohl@gmail.com>
2015-08-23 22:24:17 -0500

    archive 2

--- archive.php
+++ archive.php
@@ -2,15 +2,17 @@
  <div class="mid-wrap">
   <div class="container mid">
     <div class="row">
+    <div class="col-sm-8">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <article class="col-sm-8">
+      <article>
         <h1 class="single-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <div class="single-content"><?php the_excerpt(); ?></div>
       </article>
 <?php endwhile; endif; ?>
+    </div>
      <div class="col-sm-4 sidebar">
         <?php get_sidebar(); ?>
-      </div>
+    </div>
     </div>
   </div>
 </div>
