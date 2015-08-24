50ca80080ace96fb6cd1a9f25eb01478413ed7de
Burzmalian <gylesfohl@gmail.com>
2015-08-23 22:25:49 -0500

    index

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  <div class="mid-wrap">
   <div class="container mid">
     <div class="row">
-      <article class="col-sm-8">
+    <div class="col-sm-8">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <article>
         <h1 class="single-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <div class="single-content"><?php the_excerpt(); ?></div>
       </article>
-      <div class="col-sm-4 sidebar">
+<?php endwhile; endif; ?>
+    </div>
+     <div class="col-sm-4 sidebar">
         <?php get_sidebar(); ?>
-      </div>
+    </div>
     </div>
   </div>
 </div>
-<?php endwhile; endif; ?>
 <?php get_footer(); ?>
