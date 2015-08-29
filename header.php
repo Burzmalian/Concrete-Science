5676b86e11ee891c4a7d4b8d064138b8e522a9aa
Burzmalian <gylesfohl@gmail.com>
2015-08-29 01:49:12 -0500

    clean

--- header.php
+++ header.php
@@ -25,40 +25,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/prod/images/favicons/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/assets/prod/images/favicons/favicon-96x96.png">
   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/prod/images/favicons/favicon-16x16.png">
-  <style>
-  *,*:after,*:before{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.center{position:absolute;top:50%;left:50%;transform:translate(-50%,-50%)}html,body,div,h1,h2,h3,p,a,img,ul,li,form,label,header,nav{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}header,nav{display:block}html{margin-right:0 !important;overflow:visible !important}body{line-height:1}ul{list-style:none}img{max-width:100%;height:auto;display:block}html{background:#fff}body{font:100% Arial,sans-serif;color:#444}a{text-decoration:none;color:#005689}.form-select{width:100% !important}.form-submit{border:0}h1{font-size:40px;font-size:2.5rem;font-weight:700;line-height:1.1em;margin-bottom:15px;font-family:"Lato",sans-serif}h2{font-size:32px;font-size:2rem;line-height:1.2em;margin-bottom:15px;font-family:"Lato",sans-serif;font-weight:700}h3{font-size:26px;font-size:1.625rem;line-height:1.2em;margin-bottom:15px;font-family:"Lato",sans-serif;font-weight:700}ul{list-style:disc;margin-bottom:15px;padding-left:20px}ul
-li{font-size:18px;font-size:1.125rem;line-height:1.2em;margin-bottom:5px}.button{display:inline-block;padding:15px
-25px;border-radius:5px;color:#fff;line-height:1em;font-size:16px;font-size:1rem;font-weight:bold;text-align:center}.button.medium{padding:10px
-15px;font-size:16px;font-size:1rem}._orange{background:#f58323}.head-wrap{position:relative;background-image:url(/wp-content/themes/concrete-science/assets/prod/images/big-bubbles.png);background-image:linear-gradient(to right, #fff 0%,rgba(255,255,255,0) 100%),url(/wp-content/themes/concrete-science/assets/prod/images/big-bubbles.svg);background-position:128% 12%;background-repeat:no-repeat}.head-wrap:before,.head-wrap:after{content:" ";display:table}.head-wrap:after{clear:both}.head-wrap
-.head{padding-top:15px;padding-bottom:15px;position:relative}.head-wrap .head:before,.head-wrap .head:after{content:" ";display:table}.head-wrap .head:after{clear:both}.logo{display:block;float:left;text-indent:100%;white-space:nowrap;overflow:hidden;width:175px;height:133px;background-image:url(/wp-content/themes/concrete-science/assets/prod/images/logo.png);background-image:linear-gradient(transparent,transparent),url(/wp-content/themes/concrete-science/assets/prod/images/logo.svg);background-repeat:no-repeat}.nav-wrap{background:#555;height:47px;border-bottom:4px solid #333}.main-menu{display:block;float:left;text-align:center;list-style:none;padding:0;margin:0}.main-menu
-li{float:left;margin-bottom:0}.main-menu li:first-child{margin-left:-10px}.main-menu li
-a{display:block;padding:15px
-10px 10px;margin:0
-10px;font-size:18px;font-size:1.125rem;color:#fff;font-weight:bold;line-height:1em}.main-menu li.current-menu-item
-a{border-bottom:4px solid #f58323}.header-cta{position:absolute;top:-215px;right:12%;background:#fff;padding:30px
-13px;padding-top:80px;border-radius:50%;height:215px;width:215px;border:16px
-solid #f58323;text-align:center;-webkit-transition:top .8s;transition:top .8s}.header-cta
-h3{font-size:18px;font-size:1.125rem;font-weight:700;line-height:1.2em;margin-bottom:10px}.header-cta
-a{font-family:"Lato",sans-serif}.nav-cta{float:right;padding:4px
-10px;display:none;font-size:17px;font-size:1.0625rem;color:#fff;font-weight:bold;line-height:1em}.nav-cta
-a{margin-right:5px;font-family:"Lato",sans-serif}.hdr-active{top:-70px}.phone{position:relative;padding-left:30px}.phone:after{position:absolute;content:"";height:25px;width:25px;background:url(/wp-content/themes/concrete-science/assets/prod/images/phone-icon.png) no-repeat;left:0;top:-2px}.nav-icon{margin:0;width:25px;display:none;position:absolute;top:8px}.nav-icon:after,.nav-icon:before,.nav-icon
-div{background-color:#f6f6f6;border-radius:2px;content:'';display:block;height:3px;margin:5px
-0;-moz-transition:all .2s ease-in-out;-webkit-transition:all .2s ease-in-out;transition:all .2s ease-in-out}@media (max-width: 767px){.logo{width:135px;height:100px;background-size:100%}.hdr-active{top:-45px}.header-cta{top:-45px;right:0;background:none;border-radius:none;height:auto;width:auto;border:0}.nav-icon{display:block}.main-menu{display:none;position:absolute;top:45px;left:0;width:100%}.main-menu
-li{margin:0;width:100%}.main-menu li:first-child{margin-left:auto}.main-menu li
-a{background:#444;padding:10px
-20px;border-bottom:2px solid #333;margin:0}}.alignnone{margin:5px
-20px 20px 0}a
-img.alignnone{margin:5px
-20px 20px 0}.mfp-hide{display:none !important}.white-popup{position:relative;background:#FFF;padding:20px;width:auto;max-width:500px;margin:20px
-auto}.container{margin-right:auto;margin-left:auto;padding-left:15px;padding-right:15px}.container:before,.container:after{content:" ";display:table}.container:after{clear:both}@media (min-width: 768px){.container{width:750px}}@media (min-width: 992px){.container{width:970px}}@media (min-width: 1200px){.container{width:1170px}}.row{margin-left:-15px;margin-right:-15px}.row:before,.row:after{content:" ";display:table}.row:after{clear:both}.col-sm-4,.col-sm-12{position:relative;min-height:1px;padding-left:15px;padding-right:15px}@media (min-width: 768px){.col-sm-4,.col-sm-12{float:left}.col-sm-4{width:33.33333%}.col-sm-12{width:100%}}.hero-wrap{position:relative}.hero-image{width:100%;height:auto}.hero-text{background:rgba(51,51,51,0.8);color:#fff;text-shadow:0 -1px 0 rgba(0,0,0,0.3);height:100%}.hero-title{font-size:40px;font-size:2.5rem;font-weight:400;color:#fff;line-height:1.1em;text-shadow:1px 1px 3px rgba(0,0,0,0.5);margin-bottom:15px;font-family:"Lato",sans-serif}.hero-sub{font-size:18px;font-size:1.125rem;color:#fff;line-height:1.3em;text-shadow:1px 1px 3px rgba(0,0,0,0.5);margin-bottom:15px}@media screen and (max-width: 767px){.hero-txt-wrap{height:100%;width:100%}.hero-text{padding:10px
-50px;text-align:center}.hero-title{font-size:28px;font-size:1.75rem;line-height:1.1em;margin-bottom:10px}.hero-sub{font-size:16px;font-size:1rem;line-height:1.2em;margin-bottom:10px}.hero-sub{display:none}.hero-text
-.button{display:none}}@media screen and (min-width: 768px){.hero-txt-wrap{height:100%;width:100%}.hero-text{padding:10px
-50px;text-align:center}}@media screen and (min-width: 992px){.hero-txt-wrap{height:auto;width:auto;max-width:60%}.hero-text{padding:20px
-30px;text-align:left}}.three-spot{padding-top:40px;padding-bottom:25px}.three-spot
-h2{font-size:28px;font-size:1.75rem;font-family:"Lato",sans-serif;font-weight:700;line-height:1.2em;margin-bottom:15px}.three-spot
-p{font-size:18px;font-size:1.125rem;line-height:1.4em;margin-bottom:15px}.three-spot
-img{width:100%;height:auto}@media (max-width: 767px){.three-spot>div{margin-bottom:40px}}
-  </style>
+  
   <?php wp_head(); ?>
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
